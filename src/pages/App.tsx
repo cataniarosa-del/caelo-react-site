3fd3f3e74829eb4da399c6ed3a27acf81621b29f
cataniarosa-del <cataniarosa@gmail.com>
2026-04-30 19:36:36 +0200

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,59 +14,36 @@ import Features from "@/pages/Features";
 import Solutions from "@/pages/Solutions";
 import About from "@/pages/About";
 import Contact from "@/pages/Contact";
-import DemoAccess from "@/pages/DemoAccess";
+import Private from "@/pages/Private";
+
+import { LanguageProvider } from "@/LanguageContext";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <MotionConfig reducedMotion="user">
+    <LanguageProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <MotionConfig reducedMotion="user">
+            <Toaster />
+            <Sonner />
 
-          <Toaster />
-          <Sonner />
+            <BrowserRouter>
+              <Routes>
+                <Route path={ROUTE_PATHS.HOME} element={<Home />} />
+                <Route path={ROUTE_PATHS.FEATURES} element={<Features />} />
+                <Route path={ROUTE_PATHS.SOLUTIONS} element={<Solutions />} />
+                <Route path={ROUTE_PATHS.ABOUT} element={<About />} />
+                <Route path={ROUTE_PATHS.CONTACT} element={<Contact />} />
 
-          <BrowserRouter>
-            <Routes>
-
-              <Route
-                path={ROUTE_PATHS.HOME}
-                element={<Home />}
-              />
-
-              <Route
-                path={ROUTE_PATHS.FEATURES}
-                element={<Features />}
-              />
-
-              <Route
-                path={ROUTE_PATHS.SOLUTIONS}
-                element={<Solutions />}
-              />
-
-              <Route
-                path={ROUTE_PATHS.ABOUT}
-                element={<About />}
-              />
-
-              <Route
-                path={ROUTE_PATHS.CONTACT}
-                element={<Contact />}
-              />
-
-              {/* ACCESSO DEMO */}
-              <Route
-                path="/demo-access"
-                element={<DemoAccess />}
-              />
-
-            </Routes>
-          </BrowserRouter>
-
-        </MotionConfig>
-      </TooltipProvider>
-    </QueryClientProvider>
+                <Route path="/aziende" element={<Private />} />
+              </Routes>
+            </BrowserRouter>
+          </MotionConfig>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </LanguageProvider>
   );
 }
 
