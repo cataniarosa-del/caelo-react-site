9b23d6a2b9d2d9514357b8c2331d7117a0570e42
cataniarosa-del <cataniarosa@gmail.com>
2026-08-04 01:41:42 +0200

    Add human control article with illustration

---
+++ src/pages/ArticoloControlloUmano.tsx
@@ -0,0 +1,5 @@
+import { Layout } from "@/components/Layout";
+import { Link } from "react-router-dom";
+import { Linkedin } from "lucide-react";
+
+const heroImage = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABwUFRkVEhwZFxkgHhwiK0cuKycnK1c+QjRHZ1tta2VbZGJygKSLcnmbe2JkjsKQm6mut7m3bonJ18ey1qS0t7D/2wBDAR4gICslK1QuLlSwdWR1sLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLCwsLD/wAARCADGAWgDASIAAhEBAxEB/8QAGgAAAgMBAQ
