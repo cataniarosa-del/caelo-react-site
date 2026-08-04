42ae88f14f81b815aecab87bd28f1cbf177eee07
cataniarosa-del <cataniarosa@gmail.com>
2026-08-04 17:35:15 +0200

    Aggiunge le rotte dei verticali CAELO

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { ROUTE_PATHS } from "@/lib/index";
 import Home from "@/pages/Home";
 import Features from "@/pages/Features";
 import Solutions from "@/pages/Solutions";
+import VerticaleCaelo from "@/pages/VerticaleCaelo";
 import About from "@/pages/About";
 import Contact from "@/pages/Contact";
 import Aziende from "@/pages/Aziende";
@@ -45,36 +46,16 @@ const App = () => {
                 <Route path={ROUTE_PATHS.HOME} element={<Home />} />
                 <Route path={ROUTE_PATHS.FEATURES} element={<Features />} />
                 <Route path={ROUTE_PATHS.SOLUTIONS} element={<Solutions />} />
+                <Route path="/soluzioni/:slug" element={<VerticaleCaelo />} />
                 <Route path={ROUTE_PATHS.LA_VOCE} element={<LaVoce />} />
                 <Route path="/valutazione-indipendente" element={<ValutazioneIndipendente />} />
-                <Route
-                  path="/la-voce/quando-amministrazione-colpisce-chi-e-nel-giusto"
-                  element={<ArticoloAmministrazione />}
-                />
-                <Route
-                  path="/la-voce/linfrastruttura-della-trasparenza-oltre-la-black-box"
-                  element={<ArticoloTrasparenzaBlackBox />}
-                />
-                <Route
-                  path="/la-voce/chi-firma-deve-poter-vedere-dalla-black-box-alla-glass-box-procedurale"
-                  element={<ArticoloChiFirmaDeveVedere />}
-                />
-                <Route
-                  path="/la-voce/chi-governera-i-procedimenti-governera-il-futuro-dell-ai"
-                  element={<ArticoloGovernareProcedimenti />}
-                />
-                <Route
-                  path="/la-voce/non-basta-identificare-un-agente-ai-bisogna-governarne-lautorita"
-                  element={<ArticoloAutoritaAgentiAI />}
-                />
-                <Route
-                  path="/la-voce/il-cancello-europeo-nel-giardino-tecnologico-degli-altri"
-                  element={<ArticoloCancelloEuropeo />}
-                />
-                <Route
-                  path="/la-voce/il-controllo-umano-non-e-un-pulsante-di-emergenza"
-                  element={<ArticoloControlloUmano />}
-                />
+                <Route path="/la-voce/quando-amministrazione-colpisce-chi-e-nel-giusto" element={<ArticoloAmministrazione />} />
+                <Route path="/la-voce/linfrastruttura-della-trasparenza-oltre-la-black-box" element={<ArticoloTrasparenzaBlackBox />} />
+                <Route path="/la-voce/chi-firma-deve-poter-vedere-dalla-black-box-alla-glass-box-procedurale" element={<ArticoloChiFirmaDeveVedere />} />
+                <Route path="/la-voce/chi-governera-i-procedimenti-governera-il-futuro-dell-ai" element={<ArticoloGovernareProcedimenti />} />
+                <Route path="/la-voce/non-basta-identificare-un-agente-ai-bisogna-governarne-lautorita" element={<ArticoloAutoritaAgentiAI />} />
+                <Route path="/la-voce/il-cancello-europeo-nel-giardino-tecnologico-degli-altri" element={<ArticoloCancelloEuropeo />} />
+                <Route path="/la-voce/il-controllo-umano-non-e-un-pulsante-di-emergenza" element={<ArticoloControlloUmano />} />
                 <Route path={ROUTE_PATHS.ABOUT} element={<About />} />
                 <Route path={ROUTE_PATHS.CONTACT} element={<Contact />} />
                 <Route path="/aziende" element={<Aziende />} />
