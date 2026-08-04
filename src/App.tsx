face1ce95564ca57c3d0be90622e9416b6201849
cataniarosa-del <cataniarosa@gmail.com>
2026-08-04 17:03:59 +0200

    Collega la pagina della valutazione indipendente

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import About from "@/pages/About";
 import Contact from "@/pages/Contact";
 import Aziende from "@/pages/Aziende";
 import LaVoce from "@/pages/LaVoce";
+import ValutazioneIndipendente from "@/pages/ValutazioneIndipendente";
 import ArticoloAmministrazione from "@/pages/ArticoloAmministrazione";
 import ArticoloGovernareProcedimenti from "@/pages/ArticoloGovernareProcedimenti";
 import ArticoloAutoritaAgentiAI from "@/pages/ArticoloAutoritaAgentiAI";
@@ -45,6 +46,7 @@ const App = () => {
                 <Route path={ROUTE_PATHS.FEATURES} element={<Features />} />
                 <Route path={ROUTE_PATHS.SOLUTIONS} element={<Solutions />} />
                 <Route path={ROUTE_PATHS.LA_VOCE} element={<LaVoce />} />
+                <Route path="/valutazione-indipendente" element={<ValutazioneIndipendente />} />
                 <Route
                   path="/la-voce/quando-amministrazione-colpisce-chi-e-nel-giusto"
                   element={<ArticoloAmministrazione />}
